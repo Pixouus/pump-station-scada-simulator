8eef7df94d69ba2999c4241ade11596b9e996408
HAMDI Sidou <hamdisidahmed2@gmail.com>
2026-04-13 06:41:43 +0200

    Ajout historique des événements horodaté + screenshots

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ etat_alarme = "normal"
 
 job_cycle = None
 job_clignotement = None
+
+#  Variable de ma part 7
+dernier_etat_alarme = None
+
 # -----------------------------
 # Fonctions de contrôle
 # -----------------------------
@@ -37,12 +41,14 @@ def start_pompe():
     pompe_active = True
     systeme_demarre = True
     label_etat.config(text="Pompe ouverte", bg="green")
+    ajouter_evenement("Pompe ouverte")  # Ajout d'événement à l'historique
 
 def stop_pompe():
     global pompe_active, systeme_demarre
     pompe_active = False
     systeme_demarre = True    # Pour que le système ne commence pas automatiquement sauf si le système de démarrage est bien activé " autrement dit sans clique sur boutton le niveau ne change pas tout seul dans le début !! "
     label_etat.config(text="Pompe fermée", bg="red")
+    ajouter_evenement("Pompe fermée")  
 
 # -----------------------------
 # Fonctions de ma part 2
@@ -105,7 +111,7 @@ def mettre_en_attente():
     pompe_active = False
     systeme_demarre = False
     label_etat.config(text="Système en attente", bg="gray")
-
+    ajouter_evenement("Système mis en attente")
 
 def cycle_automatique():
     global niveau, job_cycle
@@ -127,10 +133,19 @@ def cycle_automatique():
     job_cycle = fenetre.after(1000, cycle_automatique)  # Appelle cette fonction à nouveau après 1000 ms (1 seconde)
 
 # -----------------------------
+# Fonctions de ma part 7
+# -----------------------------
+
+def ajouter_evenement(message):
+    from datetime import datetime
+    heure = datetime.now().strftime("%H:%M:%S")
+    liste_historique.insert(0, f"{heure} - {message}")
+
+# -----------------------------
 # Fonctions de ma part 4
 # -----------------------------
 def verifier_alarme():
-    global systeme_demarre, pompe_active, etat_alarme, alarme_clignotante, voyant_allume
+    global systeme_demarre, pompe_active, etat_alarme, alarme_clignotante, voyant_allume, dernier_etat_alarme
 
     if niveau >= 100:
         etat_alarme = "critique"
@@ -139,6 +154,9 @@ def verifier_alarme():
         systeme_demarre = False  # Arrêter le système en cas d'alarme critique
         pompe_active = False  # Arrêter la pompe en cas d'alarme critique
         label_etat.config(text="Système en attente", bg="gray")
+        if dernier_etat_alarme != etat_alarme:
+            ajouter_evenement("Alarme critique : niveau maximum - Arrêt automatique")
+            dernier_etat_alarme = etat_alarme # Mettre à jour le dernier état d'alarme
 
     elif niveau <= 0:
         etat_alarme = "critique"
@@ -147,6 +165,9 @@ def verifier_alarme():
         systeme_demarre = False  # Arrêter le système en cas d'alarme critique
         pompe_active = False  # Arrêter la pompe en cas d'alarme critique
         label_etat.config(text="Système en attente", bg="gray")
+        if dernier_etat_alarme != etat_alarme:
+            ajouter_evenement("Alarme critique : niveau minimum - Arrêt automatique")
+            dernier_etat_alarme = etat_alarme
 
     elif niveau >= 80:
         etat_alarme = "élevé"
@@ -154,6 +175,9 @@ def verifier_alarme():
         voyant_allume = True
         dessiner_voyant("orange")
         label_alarme.config(text="Alarme : niveau élevé")
+        if dernier_etat_alarme != etat_alarme:
+            ajouter_evenement("Alarme : niveau élevé")
+            dernier_etat_alarme = etat_alarme
 
     elif niveau <= 20:
         etat_alarme = "bas"
@@ -161,6 +185,9 @@ def verifier_alarme():
         voyant_allume = True
         dessiner_voyant("yellow")
         label_alarme.config(text="Alarme : niveau bas")
+        if dernier_etat_alarme != etat_alarme:
+            ajouter_evenement("Alarme : niveau bas")
+            dernier_etat_alarme = etat_alarme
 
     else:
         etat_alarme = "normal"
@@ -168,6 +195,9 @@ def verifier_alarme():
         voyant_allume = True
         dessiner_voyant("green")
         label_alarme.config(text="Aucune alarme")
+        if dernier_etat_alarme != etat_alarme:
+            ajouter_evenement("Retour à la normale")
+            dernier_etat_alarme = etat_alarme
 
 # -----------------------------
 # Fonctions de ma part 6
@@ -206,14 +236,42 @@ def fermer_application():
 
 fenetre = tk.Tk()  #c'est ma fenêtre pricipale et c'est exactement comme mon écran SCADA 
 fenetre.title("Supervision - Station de pompage & Cuve")
-fenetre.geometry("700x550")    # L * H
+fenetre.geometry("1000x650")    # L * H
+
+frame_gauche = tk.Frame(fenetre, padx=20, pady=10)
+frame_gauche.grid(row=0, column=0, sticky="nsew")
+
+frame_droite = tk.Frame(fenetre, padx=20, pady=10)
+frame_droite.grid(row=0, column=1, sticky="nsew")
+
+# -----------------------------
+# Historique des événements - part 7
+# -----------------------------
+
+Label_historique = tk.Label(
+    frame_droite,
+    text="Historique des événements :",
+    font=("Arial", 12, "bold")
+)
+Label_historique.pack(pady=5)
+
+frame_historique = tk.Frame(frame_droite)
+frame_historique.pack(pady=5)
+
+scrollbar = tk.Scrollbar(frame_historique)
+scrollbar.pack(side="right", fill="y")
+
+liste_historique = tk.Listbox(frame_historique, width=65, height=7, yscrollcommand=scrollbar.set, font=("Courier", 9))
+liste_historique.pack(side="left", fill="both")
+
+scrollbar.config(command=liste_historique.yview)
 
 # -----------------------------
 # Label état pompe
 # -----------------------------
 
 label_etat = tk.Label(
-    fenetre,
+    frame_gauche,
     text=" Système en attente ",
     bg="gray",
     fg="white",
@@ -227,7 +285,7 @@ label_etat.pack(pady=20) # affiche-le dans la fenêtre, sans .pack() je vois rie
 # -----------------------------
 
 label_niveau = tk.Label(
-    fenetre,
+    frame_gauche,
     text=f"Niveau : {niveau}%",
     font=("Arial", 14)
 )
@@ -246,10 +304,10 @@ label_alarme = tk.Label(
 )
 label_alarme.pack(pady=20)
 """
-canvas = tk.Canvas(fenetre, width=200, height=30, bg="white")
+canvas = tk.Canvas(frame_gauche, width=200, height=30, bg="white")
 canvas.pack(pady=10)
 
-frame_alarme = tk.Frame(fenetre)
+frame_alarme = tk.Frame(frame_gauche)
 frame_alarme.pack(pady=20)
 
 canvas_alarme = tk.Canvas(frame_alarme, width=30, height=30, highlightthickness=0)
@@ -266,7 +324,7 @@ label_alarme.pack(side="left")
 # -----------------------------
 
 btn_start = tk.Button(    # création de mon bouton
-    fenetre,
+    frame_gauche,
     text="Ouvrir",
     bg="green",
     fg="white",
@@ -276,7 +334,7 @@ btn_start = tk.Button(    # création de mon bouton
 btn_start.pack(pady=5)
 
 btn_stop = tk.Button(
-    fenetre,
+    frame_gauche,
     text="Fermer",
     bg="red",
     fg="white",
@@ -288,7 +346,7 @@ btn_stop.pack(pady=5)
 ####################### Part 2 #########################
 
 btn_remplir = tk.Button(
-    fenetre,
+    frame_gauche,
     text="Remplir",
     bg="blue",
     fg="white",
@@ -298,7 +356,7 @@ btn_remplir = tk.Button(
 btn_remplir.pack(pady=5)
 
 btn_vider = tk.Button(
-    fenetre,
+    frame_gauche,
     text="Vider",
     bg="orange",
     fg="white",
@@ -310,7 +368,7 @@ btn_vider.pack(pady=5)
 ####################### Part 3 #########################
 
 btn_attente = tk.Button(
-    fenetre,
+    frame_gauche,
     text="Attente",
     bg="gray",
     fg="white",
@@ -334,7 +392,7 @@ line, = ax.plot([], [])
 ax.set_title("Evolution du niveau")
 ax.set_ylim(0, 100)
 
-canvas_graph = FigureCanvasTkAgg(fig, master=fenetre)
+canvas_graph = FigureCanvasTkAgg(fig, master=frame_droite)
 canvas_graph.get_tk_widget().pack(pady=20)
 
 
